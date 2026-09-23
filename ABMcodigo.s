8d7b40f64a83ddc40c089c5c1effe9788e49b779
AlissonBuitrago <alissonbuitrago@unicauca.edu.co>
2026-09-22 23:24:11 -0500

    Update ABMcodigo.s

--- ABMcodigo.s
+++ ABMcodigo.s
@@ -7,13 +7,23 @@ PSECT adc_code, class=CODE, reloc=2
 
 ADC_Init:
 
-    ; RA0/AN0 como entrada para el LM35
+    ; RA0/AN0 como entrada
     BSF TRISA, 0, c
 
-    ; Pendiente:
-    ; configurar ADCON1
-    ; configurar ADCON2
-    ; configurar ADCON0
+    ; Referencias:
+    ; VREF+ = VDD
+    ; VREF- = VSS
+    ; AN0 analogico y los demas digitales
+    MOVLW 00001110B
+    MOVWF ADCON1, c
+
+    ; Resultado justificado a la izquierda
+    ; Tiempo de adquisicion = 4 TAD
+    ; Reloj ADC = Fosc/32
+    MOVLW 00010010B
+    MOVWF ADCON2, c
+
+   
 
     RETURN
 

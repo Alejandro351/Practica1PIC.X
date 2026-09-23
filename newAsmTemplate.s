2e01f4e8d2d4b0f0323a03c1467c082d222218fc
Alejandro351 <deceballos@unicauca.edu.co>
2026-09-22 22:47:50 -0500

    Configuracion Inicial

--- newAsmTemplate.s
+++ newAsmTemplate.s
@@ -1,3 +1,22 @@
+#include <xc.inc>
 
-
-
+    ;Configuracion general
+    CONFIG  FOSC   = INTOSCIO_EC
+    CONFIG  WDT    = OFF
+    CONFIG  LVP    = OFF
+    CONFIG  PBADEN = OFF
+    CONFIG  MCLRE  = ON
+    CONFIG  XINST  = OFF
+    
+    ;Inicio
+    MOVLW 01110010B ; Oscilador interno a 8 MHz
+    MOVWF OSCCON, c
+    
+    ;Botones como entradas
+    BSF TRISB, 0, c	;RB0 Alarma
+    BSF TRISB, 1 ,c	;RB1 Ventilador
+    BSF TRISB, 2, c	;RB2 Celsius a Farenheit
+    
+    ;Salidas de los botones
+    BCF TRISC, 0, c	;Led alarma
+    BCF TRISC, 1, c	;Ventilador 
